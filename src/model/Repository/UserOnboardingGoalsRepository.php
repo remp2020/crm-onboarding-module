1c667c7ec568f8213fbc0739bb408791fba53b4d
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-02-09 01:01:45 +0100

    Fix whitespaces

--- src/model/Repository/UserOnboardingGoalsRepository.php
+++ src/model/Repository/UserOnboardingGoalsRepository.php
@@ -103,24 +103,24 @@ class UserOnboardingGoalsRepository extends Repository
     final public function userRegistrationAndSubscriptionOwnershipDistributionForGoal($onboardingGoalId)
     {
         $sql=<<<SQL
-SELECT CASE 
+SELECT CASE
   WHEN days_from_registration < 1 THEN '0'
   WHEN days_from_registration = 1 THEN '1'
   WHEN days_from_registration = 2 THEN '2'
   WHEN days_from_registration < 7 THEN '3-6'
   WHEN days_from_registration < 31 THEN '7-30'
   ELSE '31+'
-END AS days_from_registration_range, tt. had_subscription, SUM(tt.total) AS total 
-FROM 
-    (SELECT TIMESTAMPDIFF(DAY, users.created_at, t.completed_at) days_from_registration, t.had_subscription, count(*) AS total 
-    FROM users 
-    JOIN 
-        (SELECT uog.user_id, uog.completed_at, CASE WHEN COUNT(s.id) > 0 THEN 1 ELSE 0 END AS had_subscription 
-        FROM user_onboarding_goals uog 
-        LEFT JOIN subscriptions s 
+END AS days_from_registration_range, tt. had_subscription, SUM(tt.total) AS total
+FROM
+    (SELECT TIMESTAMPDIFF(DAY, users.created_at, t.completed_at) days_from_registration, t.had_subscription, count(*) AS total
+    FROM users
+    JOIN
+        (SELECT uog.user_id, uog.completed_at, CASE WHEN COUNT(s.id) > 0 THEN 1 ELSE 0 END AS had_subscription
+        FROM user_onboarding_goals uog
+        LEFT JOIN subscriptions s
         ON (uog.user_id = s.user_id AND uog.completed_at >= s.start_time AND uog.completed_at <= end_time)
         WHERE uog.onboarding_goal_id = ? and uog.completed_at IS NOT NULL
-        GROUP BY uog.user_id, uog.completed_at) t 
+        GROUP BY uog.user_id, uog.completed_at) t
     ON users.id = t.user_id
     GROUP BY days_from_registration, had_subscription) tt
 GROUP BY days_from_registration_range, had_subscription
@@ -142,7 +142,7 @@ SQL;
     final public function nonSubscribersAndFirstFollowingPaymentInDaysDistributionForGoal($onboardingGoalId)
     {
         $sql=<<<SQL
-SELECT CASE 
+SELECT CASE
   WHEN first_payment_in_days IS NULL THEN '-'
   WHEN first_payment_in_days < 1 THEN '0'
   WHEN first_payment_in_days = 1 THEN '1'
@@ -151,15 +151,15 @@ SELECT CASE
   WHEN first_payment_in_days < 31 THEN '7-30'
   ELSE '31+'
 END AS first_payment_in_days_range, COUNT(user_id) AS total FROM
-    (SELECT t.user_id, MIN(TIMESTAMPDIFF(DAY, t.completed_at, p.paid_at)) AS first_payment_in_days FROM 
+    (SELECT t.user_id, MIN(TIMESTAMPDIFF(DAY, t.completed_at, p.paid_at)) AS first_payment_in_days FROM
         (SELECT uog.user_id, uog.completed_at
-        FROM user_onboarding_goals uog 
-        LEFT JOIN subscriptions s 
+        FROM user_onboarding_goals uog
+        LEFT JOIN subscriptions s
         ON (uog.user_id = s.user_id AND uog.completed_at >= s.start_time AND uog.completed_at <= end_time)
         WHERE uog.onboarding_goal_id = ? and uog.completed_at IS NOT NULL
-        GROUP BY uog.user_id, uog.completed_at 
-        HAVING COUNT(s.id)=0) t 
-    LEFT JOIN payments p ON t.user_id = p.user_id 
+        GROUP BY uog.user_id, uog.completed_at
+        HAVING COUNT(s.id)=0) t
+    LEFT JOIN payments p ON t.user_id = p.user_id
     AND p.subscription_id IS NOT NULL
     AND p.paid_at > t.completed_at
     GROUP BY t.user_id) tt
