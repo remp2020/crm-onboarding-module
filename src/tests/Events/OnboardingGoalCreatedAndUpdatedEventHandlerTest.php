745454f6e1640bea6f351cca60222d6f48137292
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-04-19 08:45:47 +0200

    Fix onboarding events handler test
    Field `segment.locked` is stored as tinyint in DB.

--- src/tests/Events/OnboardingGoalCreatedAndUpdatedEventHandlerTest.php
+++ src/tests/Events/OnboardingGoalCreatedAndUpdatedEventHandlerTest.php
@@ -72,7 +72,7 @@ class OnboardingGoalCreatedAndUpdatedEventHandlerTest extends BaseTestCase
         $this->assertEquals($segmentProperties['code'], $segmentCreated->code);
         $this->assertEquals($segmentProperties['name'], $segmentCreated->name);
         $this->assertEquals($segmentProperties['query_string'], $segmentCreated->query_string);
-        $this->assertTrue($segmentCreated->locked);
+        $this->assertTrue((bool) $segmentCreated->locked);
 
         // test update
         $onboardingGoalUpdate = clone($onboardingGoal);
@@ -94,7 +94,7 @@ class OnboardingGoalCreatedAndUpdatedEventHandlerTest extends BaseTestCase
         $this->assertEquals($segmentUpdatedProperties['code'], $segmentUpdated->code);
         $this->assertEquals($segmentUpdatedProperties['name'], $segmentUpdated->name);
         $this->assertEquals($segmentUpdatedProperties['query_string'], $segmentUpdated->query_string);
-        $this->assertTrue($segmentUpdated->locked);
+        $this->assertTrue((bool) $segmentUpdated->locked);
 
         // segment's code & query string should be same as before update
         // (code cannot be updated)
